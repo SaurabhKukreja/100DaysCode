7ba3eb311bd74d789e420106e1ebf077871d9190
Saurabh Kukreja <skukreja@wayfair.com>
2021-01-30 23:20:15 -0500

    Binary Iterative

--- src/search_algo/BinarySearch.java
+++ src/search_algo/BinarySearch.java
@@ -2,34 +2,56 @@ package search_algo;
 
 public class BinarySearch {
   /*
-  * Reason we use Binary Search is because linear Search is very expensive
-  * In real world if the array is too big, we dont want to iterate through every index to
-  * find what we want. Thats why we have Binary Search.
-  * */
+   * Reason we use Binary Search is because linear Search is very expensive
+   * In real world if the array is too big, we dont want to iterate through every index to
+   * find what we want. Thats why we have Binary Search.
+   * */
 
-  public static int binarySearchApproach1(int[] arr, int left, int right, int num) {
+  public static int binarySearchRecursive(int[] arr, int left, int right, int num) {
     /*
-    * Now because we are recursive calling the method Space Complexity = O(Log n)
-    * And because we divided the array in half in every iteration so time complexity = O(Log n )
-    * */
+     * Now because we are recursive calling the method Space Complexity = O(Log n)
+     * And because we divided the array in half in every iteration so time complexity = O(Log n )
+     * */
     if (left < right) {
       int mid = (right + left) / 2;
       if (arr[mid] == num) {
-        System.out.println("Number found at " + mid + " position");
         return mid;
       } else if (arr[mid] > num) {
-        return binarySearchApproach1(arr, left, mid - 1, num);
+        return binarySearchRecursive(arr, left, mid - 1, num);
       } else {
-        return binarySearchApproach1(arr, mid + 1, right, num);
+        return binarySearchRecursive(arr, mid + 1, right, num);
       }
     }
-    System.out.println("Sorry Number not found");
+    return -1;
+  }
+
+  public static int binarySearchIterative(int[] arr, int left, int right, int num) {
+    while (left <= right) {
+      int mid = (right + left) / 2;
+
+      if (arr[mid] == num) {
+        //Checking if arr[mid] is what we are looking for
+        return mid;
+      }
+      if (arr[mid] < num) {
+        //We search on the left half if num is smaller
+        left = mid + 1;
+      }
+      else {
+        //We search on the right half if num is greater
+        right = mid - 1;
+      }
+    }
     return -1;
   }
 
   public static void main(String[] args) {
-    int[] arr = new int[] {1,2,3,4,5,6};
-    binarySearchApproach1(arr, 0, arr.length, 30);
+    int[] arr = new int[] {1, 2, 3, 4, 5, 6};
+    int position = binarySearchRecursive(arr, 0, arr.length-1, 5);
+    System.out.println("Number found at " + position + " position");
+    int positionI = binarySearchIterative(arr, 0, arr.length-1, 5);
+    System.out.println("Number found at " + positionI + " position");
   }
+
 
 }
